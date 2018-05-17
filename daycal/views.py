0ad54b2a65419b59371bb3ca242d642b9b0ca173
Kittipat Chearapong <kittipat.chearapong@gmail.com>
2018-05-17 20:12:09 +0700

    Update

--- daycal/views.py
+++ daycal/views.py
@@ -13,25 +13,32 @@ def home(request):
         foods = foods.filter(name__icontains=search_term)
     
     if request.user.is_authenticated:
-        meals = Meal.objects.filter(user=request.user)
+        meals = Meal.objects.filter(user=request.user).order_by('-create_time')
     else:    
         meals = Noneh
     return render(request, 'daycal/home.html', {'foods':foods,'meals':meals})
 
 def addmeal(request):
+    print (request.POST)
     User = get_user_model()
     meal = Meal.objects.filter(user=request.user,create_time=datetime.strptime(request.POST['eat_date'], "%d/%m/%Y").date())
-    if meal.count() == 0:	
+    if meal.count() == 0:
         meal=Meal(user=request.user, create_time=datetime.strptime(request.POST['eat_date'], "%d/%m/%Y").date())
         meal.save()
-    selected_values = request.POST.getlist('select')
-    for selected in selected_values:
-        meal.foods.add(selected)
-    meal.save()
+        selected_values = request.POST.getlist('select')
+        for selected in selected_values:
+            meal.foods.add(selected)
+        meal.save()
+    else:
+        for meal_selected in meal:
+            selected_values = request.POST.getlist('select')
+            for selected in selected_values:
+                meal_selected.foods.add(selected)
+            meal_selected.save()
     ingredients = Ingredient.objects
     foods = Food.objects
     if request.user.is_authenticated:
-        meals = Meal.objects.filter(user=request.user)
-    else:    
-        meals = None    
-    return render(request, 'daycal/addmeal.html', {'foods':foods,'meals':meals})
+        meals = Meal.objects.filter(user=request.user).order_by('-create_time')
+    else:
+        meals = None
+    return render(request, 'daycal/addmeal.html', {'values':selected_values})
